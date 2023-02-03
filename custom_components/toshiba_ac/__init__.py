23b8e44dbcd0886715d5eec56a725ff795ce40ae
Thibaut Etienne <thibaut@etienne.pw>
2023-02-03 07:43:01 +0100

    Replace deprecated call

--- custom_components/toshiba_ac/__init__.py
+++ custom_components/toshiba_ac/__init__.py
@@ -37,12 +37,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data[DOMAIN][entry.entry_id] = device_manager
 
-    # for component in PLATFORMS:
-    #     hass.async_create_task(
-    #         hass.config_entries.async_forward_entry_setup(entry, component)
-    #     )
-
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
