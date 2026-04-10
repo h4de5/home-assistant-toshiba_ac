d9995689bbb1bcd98e3e9668b16e675371fce6c8
albaintor <118518828+albaintor@users.noreply.github.com>
2026-04-10 10:51:09 +0200

    Fix

--- custom_components/toshiba_ac/toshiba_ac/utils/http_api.py
+++ custom_components/toshiba_ac/toshiba_ac/utils/http_api.py
@@ -132,7 +132,7 @@ class ToshibaAcHttpApi:
     async def connect(self) -> None:
         headers = {
             "Content-Type": "application/json",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+            "User-Agent": self.user_agent,
         }
         post = {"Username": self.username, "Password": self.password}
 
